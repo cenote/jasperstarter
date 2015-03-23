cenote/jasperstarter

commit 42d7126a34b05d3645728a67865b26ab0296706b

JAS-67 Added ability to produce CSV Metadata Reports.

src/main/java/de/cenote/jasperstarter/App.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Cenote GmbH.
+ * Copyright 2012-2015 Cenote GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -250,12 +250,16 @@ public class App {
                 report.exportXlsx();
             } else if (OutputFormat.csv.equals(f)) {
                 report.exportCsv();
+            } else if (OutputFormat.csvMeta.equals(f)) {
+                report.exportCsvMeta();
             } else if (OutputFormat.ods.equals(f)) {
                 report.exportOds();
             } else if (OutputFormat.pptx.equals(f)) {
                 report.exportPptx();
             } else if (OutputFormat.xhtml.equals(f)) {
                 report.exportXhtml();
+            } else {
+            	throw new IllegalArgumentException("Error output format \"" + f +  "\" not implemented!");
             }
         }
         if (viewIt) {
@@ -370,7 +374,7 @@ public class App {
         ArgumentGroup groupOptions = parser.addArgumentGroup("options");
         groupOptions.addArgument("-f").metavar("<fmt>").dest(Dest.OUTPUT_FORMATS).
                 required(true).nargs("+").type(Arguments.enumType(OutputFormat.class)).
-                help("view, print, pdf, rtf, xls, xlsx, docx, odt, ods, pptx, csv, html, xhtml, xml, jrprint");
+                help("view, print, pdf, rtf, xls, xlsx, docx, odt, ods, pptx, csv, csvMeta, html, xhtml, xml, jrprint");
         groupOptions.addArgument("input").metavar("<input>").dest(Dest.INPUT).required(true).help("input file (.jrxml|.jasper|.jrprint)");
         groupOptions.addArgument("-o").metavar("<output>").dest(Dest.OUTPUT).help("directory or basename of outputfile(s)");
         //groupOptions.addArgument("-h", "--help").action(Arguments.help()).help("show this help message and exit");
@@ -414,14 +418,16 @@ public class App {
         groupDbOptions.addArgument("--csv-charset").metavar("<charset>").dest(Dest.CSV_CHARSET).setDefault("utf-8").help("CSV charset - defaults to \"utf-8\"");
         Argument argXmlXpath = groupDbOptions.addArgument("--xml-xpath").metavar("<xpath>").dest(Dest.XML_XPATH).help("XPath for XML Datasource");
 
-        ArgumentGroup groupPrintOptions = parser.addArgumentGroup("print options");
-        groupPrintOptions.addArgument("-N").metavar("<printername>").dest(Dest.PRINTER_NAME).help("name of printer");
-        groupPrintOptions.addArgument("-d").dest(Dest.WITH_PRINT_DIALOG).action(Arguments.storeTrue()).help("show print dialog when printing");
-        groupPrintOptions.addArgument("-s").metavar("<reportname>").dest(Dest.REPORT_NAME).help("set internal report/document name when printing");
-        groupPrintOptions.addArgument("-c").metavar("<copies>").dest(Dest.COPIES)
+        ArgumentGroup groupOutputOptions = parser.addArgumentGroup("output options");
+        groupOutputOptions.addArgument("-N").metavar("<printername>").dest(Dest.PRINTER_NAME).help("name of printer");
+        groupOutputOptions.addArgument("-d").dest(Dest.WITH_PRINT_DIALOG).action(Arguments.storeTrue()).help("show print dialog when printing");
+        groupOutputOptions.addArgument("-s").metavar("<reportname>").dest(Dest.REPORT_NAME).help("set internal report/document name when printing");
+        groupOutputOptions.addArgument("-c").metavar("<copies>").dest(Dest.COPIES)
                 .type(Integer.class).choices(Arguments.range(1, Integer.MAX_VALUE))
                 .help("number of copies. Defaults to 1");
-
+        groupOutputOptions.addArgument("--out-field-del").metavar("<delimiter>").dest(Dest.OUT_FIELD_DEL).setDefault(",").help("Export CSV (Metadata) Field Delimiter - defaults to \",\"");
+        groupOutputOptions.addArgument("--out-charset").metavar("<charset>").dest(Dest.OUT_CHARSET).setDefault("utf-8").help("Export CSV (Metadata) Charset - defaults to \"utf-8\"");
+        
         allArguments.put(argDbHost.getDest(), argDbHost);
         allArguments.put(argDbUser.getDest(), argDbUser);
         allArguments.put(argDbPasswd.getDest(), argDbPasswd);

src/main/java/de/cenote/jasperstarter/Config.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Cenote GmbH.
+ * Copyright 2013-2015 Cenote GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -101,7 +101,11 @@ public class Config {
     boolean writeJasper;
     @Arg(dest = Dest.COPIES)
     Integer copies;
-
+    @Arg(dest = Dest.OUT_FIELD_DEL)
+    String outFieldDel;
+    @Arg(dest = Dest.OUT_CHARSET)
+    String outCharset;
+    
     // end of argparse4j arguments
     /**
      *
@@ -346,7 +350,13 @@ public class Config {
             return false;
         }
     }
-    
+    public String getOutFieldDel() {
+        return outFieldDel;
+    }
+    public String getOutCharset() {
+        return outCharset;
+    }
+
     // END argparse4j arguments
-    // @todo: overwrite toString()
+    // TODO: overwrite toString()
 }

src/main/java/de/cenote/jasperstarter/Report.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Cenote GmbH.
+ * Copyright 2012-2015 Cenote GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,6 +63,7 @@ import net.sf.jasperreports.engine.data.JRXmlDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.export.HtmlExporter;
 import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRCsvMetadataExporter;
 import net.sf.jasperreports.engine.export.JRPrintServiceExporter;
 import net.sf.jasperreports.engine.export.JRRtfExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
@@ -74,6 +75,8 @@ import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.engine.util.JRSaver;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.export.SimpleCsvExporterConfiguration;
+import net.sf.jasperreports.export.SimpleCsvMetadataExporterConfiguration;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleHtmlExporterConfiguration;
 import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
@@ -384,11 +387,26 @@ public class Report {
 
     public void exportCsv() throws JRException {
         JRCsvExporter exporter = new JRCsvExporter();
+        SimpleCsvExporterConfiguration configuration = new SimpleCsvExporterConfiguration();
+        configuration.setFieldDelimiter(config.getOutFieldDel());
+        exporter.setConfiguration(configuration);
         exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
 		exporter.setExporterOutput(new SimpleWriterExporterOutput(
-				this.output.getAbsolutePath() + ".csv")); 
+				this.output.getAbsolutePath() + ".csv", config.getOutCharset()));
         exporter.exportReport();
     }
+
+    // the CSV Metadata Exporter
+    public void exportCsvMeta() throws JRException {
+    	JRCsvMetadataExporter exporter = new JRCsvMetadataExporter();
+    	SimpleCsvMetadataExporterConfiguration configuration = new SimpleCsvMetadataExporterConfiguration();
+    	configuration.setFieldDelimiter(config.getOutFieldDel());
+    	exporter.setConfiguration(configuration);
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleWriterExporterOutput(
+				this.output.getAbsolutePath() + ".csv", config.getOutCharset()));
+        exporter.exportReport();
+    }    
 
     public void exportOds() throws JRException {
         JROdsExporter exporter = new JROdsExporter();

src/main/java/de/cenote/jasperstarter/types/Dest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Cenote GmbH.
+ * Copyright 2012-2015 Cenote GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,8 @@ public interface Dest {
     public static String CSV_FIELD_DEL = "csv-field-del";   // --csv-field-del
     public static String CSV_CHARSET = "csv-charset";       // --csv-charset
     public static String XML_XPATH = "xml-xpath";           // --xml-xpath
+    public static String OUT_FIELD_DEL = "out-field-del"; // --out-field-del
+    public static String OUT_CHARSET = "out-charset";       // --out-charset
     public static String PRINTER_NAME = "printer-name";     // -N
     public static String WITH_PRINT_DIALOG = "with-print-dialog"; // -d
     public static String REPORT_NAME = "set-report-name";   // -s

src/main/java/de/cenote/jasperstarter/types/OutputFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Cenote GmbH.
+ * Copyright 2012-2015 Cenote GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ public enum OutputFormat {
     // jxl,          // not implemented jet
     // jxlMetadata,  // not implemented jet
     csv,
-    // csvMetadata,  // not implemented jet
+    csvMeta,
     ods,
     pptx,
     xhtml;

commit 0c6715152b135f649371964a38390230a3ea152b

JAS-67 Fixed a bug introduced with change 42d7126.

src/main/java/de/cenote/jasperstarter/App.java
@@ -258,6 +258,8 @@ public class App {
                 report.exportPptx();
             } else if (OutputFormat.xhtml.equals(f)) {
                 report.exportXhtml();
+            } else if (OutputFormat.jrprint.equals(f)) {
+            	// nothing to do. Option is used in Report.fill()
             } else {
             	throw new IllegalArgumentException("Error output format \"" + f +  "\" not implemented!");
             }